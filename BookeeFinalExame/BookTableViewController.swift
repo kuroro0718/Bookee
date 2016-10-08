5c92d69eca0ecd0619dc7315fe1b8bffbdff9538
Alex Liang <u9211011@gmail.com>
2016-10-08 19:26:05 +0800

    Load book data from Firebase while APP lunched

--- BookeeFinalExame/BookTableViewController.swift
+++ BookeeFinalExame/BookTableViewController.swift
@@ -13,12 +13,16 @@ class BookTableViewController: UITableViewController {
     @IBOutlet var bookTableView: UITableView!
     
     var books = [Book]()
+    var rootRef: FIRDatabaseReference?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         FIRApp.configure()
-        initBooks()
-//        loadDataFromFirebase()
+        rootRef = FIRDatabase.database().reference()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        loadDataFromFirebase()
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,51 +30,30 @@ class BookTableViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    // MARK: - Table view data source
-    
-//    func loadDataFromFirebase() {
-//        let rootRef = FIRDatabase.database().reference()
-//        
-//        rootRef.child("Book").observeEventType(.Value, withBlock: {
-//            (snapshot) in
-//            let dict = snapshot.value
-//            print(dict)
-//        })
-//
-//    }
-    
-    func initBooks() {
-        let book1 = Book(name: "莊子", shopAddress: "台北市松山區復興南路一段39號", shopPhoneNum: 26535588, shopWebSite: "http://ppt.cc/NWvFW", introduction: "身心的安定，是面對混亂的時代最有力量的武器，以淺顯的語言，契合現代生活的事例，解讀莊子的治身心之道", imageName: "chung")
-     
-        let book2 = Book(name: "地獄", shopAddress: "台北市中山區復興北路386號", shopPhoneNum: 24535588, shopWebSite: "http://ppt.cc/P5y3k", introduction: "地獄最黑暗的地方，保留給那些在道德存亡之際袖手旁觀的人。", imageName: "hell")
-        
-        let book3 = Book(name: "這樣吃，體能回到20歲", shopAddress: "台北市大安區敦化南路一段245號", shopPhoneNum: 26535587, shopWebSite: "http://ppt.cc/k3x76", introduction: "現在的我，一動就累，連出門抓個寶可夢也氣喘吁吁，怎麼辦？", imageName: "health")
-        
-        let book4 = Book(name: "大地療癒力", shopAddress: "台北市大安區敦化南路一段245號", shopPhoneNum: 26535688, shopWebSite: "http://ppt.cc/bw1CC", introduction: "大地是孩子最好的醫生！拒絕各種慢性病、文明病、遺傳病，就從讓孩子「吃對」食物開始！", imageName: "ground")
-        
-        let book5 = Book(name: "弘一大師的超脫之學", shopAddress: "台北市大安區敦化南路一段245號", shopPhoneNum: 26235688, shopWebSite: "http://ppt.cc/ppdkA", introduction: "弘一法師 - 李叔同:「長亭外，古道邊，芳草碧連天」", imageName: "master")
-        
-        books.append(book1)
-        books.append(book2)
-        books.append(book3)
-        books.append(book4)
-        books.append(book5)
-    }
-    
-    func uploadBookInfoToFirebase() {
-        let rootRef = FIRDatabase.database().reference()
-        let bookRef = rootRef.child("Book")
-                
-        for book in books {
-            let autoRef = bookRef.childByAutoId()
-            let detailRef = autoRef.child(book.name!)
-            
-            detailRef.child("書本介紹").setValue(book.introduction!)
-            detailRef.child("書本介紹").setValue(book.imageName!)
-            detailRef.child("書局地址").setValue(book.shopAddress!)
-            detailRef.child("書局電話").setValue(book.shopPhoneNum)
-            detailRef.child("書局網站").setValue(book.shopWebSite!)
-        }
+    func loadDataFromFirebase() {
+        let bookRef = self.rootRef!.child("Book")
+        bookRef.observeSingleEventOfType(.Value, withBlock:  {
+            (snapshot) in
+            if let result = snapshot.children.allObjects as? [FIRDataSnapshot] {
+                for child in result {
+                    let dataDict = child.value as! [String: AnyObject]
+                    for (bookName, bookDict) in dataDict {
+                        let bookData = bookDict as! [String: AnyObject]
+                        
+                        let address = bookData["shop address"]! as! String
+                        let phoneNum = bookData["shop phone"]! as! Int
+                        let shopWebsite = bookData["shop website"]! as! String
+                        let introduction = bookData["introduction"]! as! String
+                        let imageName = bookData["cover"]! as! String
+                        
+                        let book = Book(name: bookName, shopAddress: address, shopPhoneNum: phoneNum, shopWebSite: shopWebsite, introduction: introduction, imageName: imageName)
+                        
+                        self.books.append(book)
+                        self.tableView.reloadData()
+                    }
+                }
+            }
+        })
     }
     
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
